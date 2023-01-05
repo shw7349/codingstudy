47b04e5a7769dd42ac2a78146596de85e3dbc715
HyunWoo <shw7349@naver.com>
2023-01-05 23:47:44 +0900

    2022.01.05

---
+++ study/문자열을 정수로 바꾸기.java
@@ -0,0 +1,29 @@
+/*class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}*/
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+class Solution {
+    public int getStrToInt(String str) {
+            boolean Sign = true;
+            int result = 0;
+
+      for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (ch == '-')
+                    Sign = false;
+                else if(ch !='+')
+                    result = result * 10 + (ch - '0');
+            }
+            return Sign?1:-1 * result;
+    }
+    //아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String args[]) {
+        StrToInt strToInt = new StrToInt();
+        System.out.println(strToInt.getStrToInt("-1234"));
+    }
+}
+
