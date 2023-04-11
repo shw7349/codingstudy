0ba67c9d9dff07cd1ee99b42fdeceeda3906a1ba
HyunWoo <shw7349@naver.com>
2023-04-11 09:28:33 +0900

    1543번 문서검색

--- study/단어공부.java
+++ study/단어공부.java
@@ -13,7 +13,7 @@ class 단어공부 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.next().toUpperCase();
         int[] count = getAlphabetCount(str);
         /*for (int i = 0; i < 26; i++)
             System.out.println((char)('A' + i) +": " + count[i]);
