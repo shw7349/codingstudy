cdeeaa903eaac3ed01e2afea463df56ec3b9f56a
HyunWoo <shw7349@naver.com>
2023-04-17 23:22:54 +0900

    [백준]10431 줄세우기

--- study/test.java
+++ study/test.java
@@ -1,15 +1,9 @@
 public class test {
-    public void DFS(int n){
-        if(n==0) return;
-        else{
-            System.out.print(n%2+" ");
-            DFS(n/2);
-        }
-    }
 
     public static void main(String[] args){
-        test T = new test();
-        T.DFS(11);
+        int T= 10;
+        while(T++ <20)
+        System.out.println(T);
     }
     
 }
