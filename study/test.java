c11b49c85744153d6f5b9a178feaf8d8bf815ab0
HyunWoo <shw7349@naver.com>
2023-04-27 18:32:53 +0900

    [백준]2839번 - 설탕배달

--- study/test.java
+++ study/test.java
@@ -3,24 +3,23 @@ public class test {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int total = 0;
-        boolean [][] square = new boolean[101][101];
-
-        for (int i = 0; i < a; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            for (int j = x; j < x+10; j++) {
-                for (int k = y; k < y+10; k++) {
-                    if (!square[j][k]) {
-                        square[j][k] = true;
-                        total++;
-                    }
-                }
-            }
-
+        int answer = 0;
+        while(true){
+        if(a%5==0){
+            System.out.println(a/5 + answer);
+            break;
+        }else if(a < 0){
+            System.out.println(-1);
+            break;
         }
-        System.out.print(total);
+        a = a-3;
+        answer ++;
+        }
+        
+        
+        
         
     }
+
 
 }
---
+++ study/진법변환반대.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class 진법변환반대 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        //int b = sc.nextInt();
+        
+        for (int i = 0; i < a; i++) {
+            char c = a.charAt(i);
+        }
+        System.out.println("c::"+c);
+        
+        
+        
+    }
+    
+}
