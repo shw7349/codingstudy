b5289a1c9f9b25d52d7373cd7fff2f8e8b207ec2
HyunWoo <shw7349@naver.com>
2023-04-30 16:21:56 +0900

    [백준]3009번- 네번째점

--- study/test.java
+++ study/test.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args){    
-        String myString = "AbCdEfG";
-        String pat = "afc";
-        int answer = 0;
-        myString = myString.toLowerCase();
-        pat = pat.toLowerCase();
-        boolean result = myString.contains(pat);
-        if(result==true){
-            answer = 1;
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr1 = {sc.nextInt(),sc.nextInt()};
+        int [] arr2 = {sc.nextInt(),sc.nextInt()};
+        int [] arr3 = {sc.nextInt(),sc.nextInt()};
+
+        int a = 0;
+        int b = 0;
+
+        if(arr1[0]==arr2[0]){
+            a = arr3[0];
+        }else if(arr2[0]==arr3[0]){
+            a = arr1[0];
         }else{
-            answer = 0;
+            a = arr2[0];
         }
-        System.out.println(result);
-        System.out.println(answer);
+
+        if(arr1[1]==arr2[1]){
+            b = arr3[1];
+        }else if(arr2[1]==arr3[1]){
+            b = arr1[1];
+        }else{
+            b = arr2[1];
+        }
+
+        
+        System.out.println(a + " " + b);
     }
 }
