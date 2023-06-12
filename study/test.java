2e444814510a976e6f7e068c732e8346ae3cfa09
HyunWoo <shw7349@naver.com>
2023-06-12 22:52:10 +0900

    [프로그래머스] 숫자짝꿍

--- study/test.java
+++ study/test.java
@@ -4,9 +4,7 @@ public class test {
     public static void main(String[] args){    
         Scanner sc = new Scanner(System.in);
 
-        String a = sc.nextLine();
-        Long result = Long.parseLong(a);
-
-        System.out.println(4 * a);
+        long n = sc.nextLong();
+        System.out.println(n*4);
     }
 }
